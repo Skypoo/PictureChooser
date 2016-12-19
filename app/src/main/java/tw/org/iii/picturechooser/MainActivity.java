d31e01c8f80ceb57d61d8021be4e28dca9aaecdb
Skypoo <poopooalen@gmail.com>
2016-12-19 23:16:19 +0800

    圖片選取\data判斷式\相機拍照

--- app/src/main/java/tw/org/iii/picturechooser/MainActivity.java
+++ app/src/main/java/tw/org/iii/picturechooser/MainActivity.java
@@ -9,7 +9,9 @@ import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.os.PersistableBundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,30 +22,63 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button picker;
+    Button picker,camera;
     private static final int REQUEST_EXTERNAL_STORAGE = 200;
+    File tmpFile;
+    ImageView imageView;
+    Uri tmpFileUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = (ImageView) findViewById(R.id.imageView);
+        camera = (Button)findViewById(R.id.camera);
+        picker = (Button)findViewById(R.id.chooser);
+
+
+
         if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED )
         {
             //未取得權限，向使用者要求允許權限
             ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.CAMERA},
                     REQUEST_EXTERNAL_STORAGE);
         }
         else
         {
-            pictureChooser();
+
         }
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v("brad","initCamera Click");
+                initCamera();
+            }
+        });
+
+        picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initPictureChooser();
+                Log.v("brad","initPictureChooser Click");
+            }
+        });
+
     }
 
     @Override
@@ -55,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-                    pictureChooser();
+                    Log.v("brad","onRequestPermissionResult");
                 } else {
                     finish();
                     // permission denied, boo! Disable the
@@ -78,44 +113,89 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void pictureChooser (){
-        picker = (Button)findViewById(R.id.chooser);
+    public void initCamera(){
+        Log.v("brad","initCamera");
+//        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
 
-        picker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        tmpFile = new File(
+                Environment.getExternalStorageDirectory(),"image.jpg");
+        tmpFileUri = Uri.fromFile(tmpFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, tmpFileUri);
+
+        startActivityForResult(intent,200);
+        Log.v("brad",intent.toString());
+    }
+
+
+    public void initPictureChooser (){
+        Log.v("brad","initPictureChooser");
                 Intent picker = new Intent(Intent.ACTION_GET_CONTENT);
                 picker.setType("image/*");
                 picker.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
                 Intent desIntent = Intent.createChooser(picker,null);
                 startActivityForResult(desIntent,100);
-            }
-        });
-
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 100){
-            if(resultCode == Activity.RESULT_OK){
-                if (data !=null) {
-                    Uri uri = data.getData();
-                    ContentResolver cr = this.getContentResolver();
-                    try {
-                        Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
-                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                        imageView.setImageBitmap(bitmap);
-                    } catch (Exception e) {
-                        Log.v("brad", e.toString());
-                    }
-                }else{
-                    Toast.makeText(this,"無法找到檔案",Toast.LENGTH_LONG).show();
+        if(resultCode == Activity.RESULT_OK){
+            Log.v("brad","RESULT_OK");
+//            if (data !=null) {
+                switch (requestCode){
+                    case 100:
+                        Log.v("brad","case 100");
+                        if (data !=null) {
+                            Uri uri = data.getData();
+                            ContentResolver cr = this.getContentResolver();
+                            try {
+                                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                                imageView.setImageBitmap(bitmap);
+                            } catch (Exception e) {
+                                Log.v("brad", e.toString());
+                            }
+                        }
+
+                        else{
+                            Toast.makeText(this,"無法找到檔案",Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case 200:
+                        if (data !=null) {
+                            Bitmap bmp = (Bitmap)data.getExtras().get("data");
+                            imageView.setImageBitmap(bmp);
+
+//                            Bitmap bmp = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+ "/image.jpg");
+//                            imageView.setImageBitmap(bmp);
+                        }else{
+                            if(tmpFile.exists()){
+                                Log.v("brad",tmpFile.getAbsolutePath().toString());
+
+                                Bitmap bmp = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+File.separator+"image.jpg");
+                                imageView.setImageBitmap(bmp);
+//                                tmpFile.mkdir();
+                            }else{
+                                Toast.makeText(this,"無法找到檔案",Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        break;
                 }
-
-            }
+//                if(requestCode == 100){
+//                    Uri uri = data.getData();
+//                    ContentResolver cr = this.getContentResolver();
+//                    try {
+//                        Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+//                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//                        imageView.setImageBitmap(bitmap);
+//                    } catch (Exception e) {
+//                        Log.v("brad", e.toString());
+//                    }
+//                }
+//            }else{
+//                Toast.makeText(this,"無法找到檔案",Toast.LENGTH_LONG).show();
+//            }
         }
-
     }
 }
